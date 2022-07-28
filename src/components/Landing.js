697f341683bd1eddf2f7ffe565f0790e81143fa0
ibtechraj <krs30018@gmail.com>
2022-07-28 19:23:17 +0530

    image error fixed

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -22,6 +22,19 @@ function Landing(props) {
   // const salonurl = 'http://localhost:3001/salons';
   // const salonurl = 'https://groomserver.herokuapp.com/salons';
 
+  // const getSalonData = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       salonUrl
+  //     );
+  //     setSalons(response.data);
+  //     console.log('respd', response.data)
+  //   } catch (err) {
+  //     console.log('e', err.message)
+  //     setSalons(null);
+  //   } 
+  // }
+
   useEffect(() => {
     axios.get(salonUrl,
     )
@@ -29,6 +42,8 @@ function Landing(props) {
         setSalons(data)
          console.log('cnt', salons.count) 
       })
+    
+  //  getSalonData()
   }, [])
 
   return (
